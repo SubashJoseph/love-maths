a1b007313182b30ba547dcd8591c5d1ed151e16b
SubashJoseph <orapuzhi@gmail.com>
2022-06-29 20:29:10 +0000

    add event listerns for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!");
+            } else {
+                    let gmeType = this.getAttribute("data-type");
+                    alert('you clicked ${gameType}');
+                
+            }
+        })
+    }s
+})
+
 function runGame() {
 
 }
